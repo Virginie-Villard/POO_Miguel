86173e7865e2961f390ff7a041cbecdedea1ff48
Virginie-Villard <virginie.villard@le-campus-numerique.fr>
2021-09-13 16:30:55 +0200

    usage après remédiation avec Miguel

--- usages.php
+++ usages.php
@@ -2,28 +2,28 @@
 
 // création de $perso1
 $perso1 = new Personnage(50,0);
-$perso1->print_rObject('$perso_1');
-$perso1->speak('Bonjour, je m\'appelle '.$name);
+var_dump($perso1);
+/*
+$perso1->parler('Bonjour, je m\'appelle perso_1');
 echo '<br>Expérience de perso_1 apprès gain d\'expérience : ';
-$perso1->gainExperience(); // On gagne de l'expérience.
-$perso1->displayExperience(); // On affiche la nouvelle valeur de l'attribut.
-
-
-
-
-
+$perso1->gagnerExperience(); // On gagne de l'expérience.
+$perso1->afficherExperience(); // On affiche la nouvelle valeur de l'attribut.
+*/
 // création de $perso2
-$perso2 = new Personnage(50,0);
-$perso2->print_rObject('$perso_2');
-$perso2->parler('Bonjour, je m\'appelle perso_2');
+$perso2 = new Personnage(200,0);
+var_dump($perso2);
 
-
-
+$perso3 = new Personnage(200,0);
+var_dump($perso3);
+/*$perso2->parler('Bonjour, je m\'appelle perso_2');
 
 
 // $perso1 frappe $perso2
 $perso1->frapper($perso2);
-echo 'Dégats de perso_2 après le coup: '. $perso2-
->degats().'</br>';
+echo 'Dégats de perso_2 après le coup: '. $perso2->degats().'</br>';
+*/
+
+echo Personnage::nbObjects().' personnages instanciés';
+//echo $perso1->getCompteurCoups();
 
 ?>
