27a91f55df9363747a4eb22fdafaabe4686cd94a
Virginie-Villard <virginie.villard@le-campus-numerique.fr>
2021-09-13 16:37:17 +0200

    index après remédiation avec Miguel

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
-require 'Personnage.php'; // On utilise la classe.
+// On crée une pile d'autoload avec le spl_autoload
+spl_autoload_register('requireClass');
+
+// On crée une fonction pour faire les require de Classe
+function requireClass($classe){
+    require $classe . '.php';
+}
 
 include 'usages.php'; // On inclut les usages.
 
